923740936b4e2ccedef770af34259afd54c41540
n0n4 <rkynona@gmail.com>
2020-03-30 03:10:31 -0400

    new defn/obj unit test

--- RelaScript.UT/ClassTests.cs
+++ RelaScript.UT/ClassTests.cs
@@ -387,5 +387,37 @@ namespace RelaScript.UT
                 new InputVar("v:y", 10.0)
             });
         }
+
+        [TestMethod]
+        public void DefnPriorScopeMultipleObjectsTest()
+        {
+            TestScaffold.TestLines(
+            lines: new List<string>()
+            {
+                "f:aaa := { 0 } \r\n" +
+                "defn d:point {\r\n" +
+                "    v:x := 0\r\n" +
+                "    v:y := 0\r\n" +
+                "    f:init := {\r\n" +
+                "        a:x a:y\r\n" +
+                "        v:x = a:x\r\n" +
+                "        v:y = a:y\r\n" +
+                "    }\r\n" +
+                "    f:array := { v:x, v:y }\r\n" +
+                "}\r\n" +
+                "\r\n" +
+                "v:point1 := object anon d:point (5, 10)\r\n" +
+                "v:point2 := object anon d:point (3, 4)\r\n" +
+                "v:point1.f:array()[0]",
+                "v:point1.f:array()[1]",
+                "v:point2.f:array()[1]"
+            },
+            expected: new List<object>()
+            {
+                new InputVar("v:x", 5.0),
+                new InputVar("v:y", 10.0),
+                new InputVar("v:y", 4.0)
+            });
+        }
     }
 }
